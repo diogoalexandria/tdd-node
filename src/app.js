daf755771ba20de9beb71825e18c1a3a9e32a58a
Diogo Agra Alexandria <diogoagrasmsdc@gmail.com>
2020-01-29 17:15:30 -0300

    Primeiro commit

---
+++ src/app.js
@@ -0,0 +1,20 @@
+express = require('express');
+
+class AppController {
+    constructor() {
+        this.express = express();
+
+        this.middlewares();
+        this.routes();        
+    }
+
+    middlewares() {
+        // this.express.use(this.express.json())
+    }
+
+    routes() {
+        // this.express.use(require('./routes'))
+    }
+}
+
+module.exports = new AppController().express;
---
+++ src/config/database.js
@@ -0,0 +1,16 @@
+module.exports = {
+    host: '127.0.0.1',
+    username: 'docker',
+    password: 'docker',
+    database: 'nodeauth',
+    dialect: 'postgres',
+    operatorsAliases: false,
+    logging: false,
+    define: {
+        timestamp: true,
+        underscored: true,
+        underscoredAll: true,
+    }
+};
+
+UserGroup
